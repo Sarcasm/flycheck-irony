dd4d1c19753c73c791db829048b05dae4b9e1b48
Guillaume Papin <guillaume.papin@epitech.eu>
2015-01-06 00:39:20 +0100

    add support for verification results
    Closes #1.

--- flycheck-irony.el
+++ flycheck-irony.el
@@ -1,12 +1,12 @@
 ;;; flycheck-irony.el --- Flycheck: C/C++ support via Irony  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014  Guillaume Papin
+;; Copyright (C) 2014-2015  Guillaume Papin
 
 ;; Author: Guillaume Papin <guillaume.papin@epitech.eu>
 ;; Keywords: convenience, tools, c
 ;; Version: 0.1.0-cvs
 ;; URL: https://github.com/Sarcasm/flycheck-irony/
-;; Package-Requires: ((emacs "24.1") (flycheck "0.22-cvs1") (irony "0.2.0-cvs2"))
+;; Package-Requires: ((emacs "24.1") (flycheck "0.22") (irony "0.2.0-cvs2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -63,9 +63,29 @@
                         (irony-diagnostics))))
            (funcall callback 'finished (delq nil errors)))))))
 
+(defun flycheck-irony--verify (checker)
+  "Verify the Flycheck Irony syntax checker."
+  (list
+   (flycheck-verification-result-new
+    :label "Irony Mode"
+    :message (if irony-mode "enabled" "disabled")
+    :face (if irony-mode 'success '(bold warning)))
+
+   ;; FIXME: the logic of `irony--locate-server-executable' could be extracted
+   ;; into something very useful for this verification
+   (let* ((server-path (expand-file-name "bin/irony-server"
+                                         irony-server-install-prefix))
+          (server-found (file-exists-p server-path)))
+     (flycheck-verification-result-new
+      :label "irony-server"
+      :message (if server-found (format "Found at %s" server-path) "Not found")
+      :face (if server-found 'success '(bold error))))))
+
 (flycheck-define-generic-checker 'irony
   "A syntax checker for C, C++ and Objective-C, using Irony Mode."
-  :start 'flycheck-irony--start
+  :start #'flycheck-irony--start
+  :verify #'flycheck-irony--verify
+  :modes irony-supported-major-modes
   :error-filter #'identity
   :predicate #'(lambda ()
                  irony-mode))
